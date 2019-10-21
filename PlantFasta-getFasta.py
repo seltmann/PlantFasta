fc1203b3cd9c4091060e8b7308fd15a3e42740ed
seltmann <enicospilus@gmail.com>
2019-10-21 15:35:33 +0200

    download fasta files based on gene and plant list

--- PlantFasta-getFasta.py
+++ PlantFasta-getFasta.py
@@ -2,7 +2,7 @@
 # K. Seltmann
 # takes a list of names and returns the number of records for a specific gene region based on the plant list
 # requires Cal-plants.txt
-# to change the gene, change gene region mentioned in for loop (line31)
+# to change the gene, change gene region mentioned in for loop (line31), downloads fasta files based on a list of plants and gene region
 
 #import Entrez
 from Bio import Entrez
@@ -16,6 +16,14 @@ def countGene(name, gene):
     record = Entrez.read(handle)
     print(record)
     return record
+
+#function to return fasta file based on single ID
+def singleEntry(singleID):   #the singleID is the accession number
+    handle = Entrez.efetch(db='nucleotide',id=singleID, rettype = 'fasta', retmode= 'text')
+    f = open('%s.fasta' % singleID, 'w')
+    f.write(handle.read())
+    handle.close()
+    f.close()
 	
 # get list of plant names and put it in a list
 text_file = open("Cal-plants.txt", "r")
@@ -24,15 +32,19 @@ print(lines)
 text_file.close()
 
 # open a file to write to
-f = open("plantCounts.txt", "w")
+f = open("plantCounts-downloadFasta.txt", "w")
 
-#go through list and pass to countGene function 
+#go through list and pass to countGene function
 for plantNames in lines:
     name = plantNames
     gene = 'rbcl' #change gene region here
     record = countGene(name,gene)
+    genes = record["IdList"]
+    for g in genes:
+        singleEntry(g)
+    print(genes)
     recordRow = (name.strip(),",", gene,",",record["Count"]+'\n')
-    print(recordRow)
+    #print(record)
     f.writelines(recordRow)
 	
 #close file
